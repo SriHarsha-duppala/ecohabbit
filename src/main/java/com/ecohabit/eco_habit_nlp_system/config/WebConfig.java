c710da360a2ff884f0b4251b5877ede45ef722de
SriHarsha-duppala <129260957+SriHarsha-duppala@users.noreply.github.com>
2026-01-12 14:30:27 +0530

    Update WebConfig.java

--- src/main/java/com/ecohabit/eco_habit_nlp_system/config/WebConfig.java
+++ src/main/java/com/ecohabit/eco_habit_nlp_system/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://ecohabit-nlp.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
+
 
