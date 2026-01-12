e99ca118ca8a0375fb2c4e16291a79d5c08aa461
SriHarsha-duppala <129260957+SriHarsha-duppala@users.noreply.github.com>
2026-01-12 12:57:23 +0530

    Update CarbonFootprintCalculator.java

--- src/main/java/com/ecohabit/eco_habit_nlp_system/utils/CarbonFootprintCalculator.java
+++ src/main/java/com/ecohabit/eco_habit_nlp_system/utils/CarbonFootprintCalculator.java
@@ -1,21 +1,24 @@
 package com.ecohabit.eco_habit_nlp_system.utils;
 
 import org.springframework.stereotype.Component;
-
 import java.util.Map;
 
 @Component
 public class CarbonFootprintCalculator {
 
     private static final Map<String, Double> carbonMap = Map.of(
-            "transport", 2.5,   // kg CO₂
-            "plastic", 1.8,
-            "energy", 3.2,
-            "water", 1.0,
-            "general", 0.5
+
+            // ❌ High pollution habits
+            "transport", 3.0,   // car, bus
+            "energy", 2.5,      // electricity, AC
+            "plastic", 2.0,     // plastic usage
+
+            // ✅ Low pollution habits
+            "water", 1.0,       // water saving
+            "general", 0.8      // neutral
     );
 
     public double calculate(String category) {
-        return carbonMap.getOrDefault(category, 0.5);
+        return carbonMap.getOrDefault(category, 0.8);
     }
 }
